98a121eccc12991873fb8b4c93c7440ae0e11e1e
simon <brotherlogic@gmail.com>
2021-10-19 18:27:28 -0700

    Enable health checking

--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ go 1.16
 require (
 	github.com/brotherlogic/discovery v0.0.0-20210817224717-ebf146aae909
 	github.com/brotherlogic/goserver v0.0.0-20211016233716-06870779572e
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f
+	github.com/brotherlogic/lock v0.0.0-20211018000640-77ca8dec85fb // indirect
+	github.com/prometheus/client_golang v1.11.0
+	golang.org/x/net v0.0.0-20211019232329-c6ed85c7a12d
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	google.golang.org/genproto v0.0.0-20211019152133-63b7e35f4404 // indirect
 	google.golang.org/grpc v1.41.0
 )
--- healthcheckerutils.go
+++ healthcheckerutils.go
@@ -4,9 +4,19 @@ import (
 	"fmt"
 
 	"golang.org/x/net/context"
+	"google.golang.org/grpc/status"
 
 	dpb "github.com/brotherlogic/discovery/proto"
 	gpb "github.com/brotherlogic/goserver/proto"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	healthChecks = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "healthchecker_checks",
+		Help: "The number of server requests",
+	}, []string{"service", "identifier", "error"})
 )
 
 func (s *Server) checkHealth(ctx context.Context, server *dpb.RegistryEntry) error {
@@ -18,12 +28,27 @@ func (s *Server) checkHealth(ctx context.Context, server *dpb.RegistryEntry) err
 	client := gpb.NewGoserverServiceClient(conn)
 	alive, err := client.IsAlive(ctx, &gpb.Alive{})
 	if err != nil {
+		healthChecks.With(prometheus.Labels{
+			"service":    server.GetName(),
+			"identifier": server.GetIdentifier(),
+			"error":      fmt.Sprintf("%v", status.Convert(err).Code()),
+		}).Inc()
 		return err
 	}
 
 	if alive.GetName() == server.GetName() {
+		healthChecks.With(prometheus.Labels{
+			"service":    server.GetName(),
+			"identifier": server.GetIdentifier(),
+			"error":      "nil",
+		}).Inc()
 		return nil
 	}
 
+	healthChecks.With(prometheus.Labels{
+		"service":    server.GetName(),
+		"identifier": server.GetIdentifier(),
+		"error":      "unknown",
+	}).Inc()
 	return fmt.Errorf("Unable to determine if %v is alive -> %v, %v", server, alive, err)
 }
